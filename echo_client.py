96ad70f705d9beb7581f693cd0b07d9d4f7bdc1c
lich97zz <136956344@qq.com>
2021-10-03 19:43:54 -0500

    test

--- echo_client.py
+++ echo_client.py
@@ -1,14 +1,28 @@
 import socket
 import os
+import time
 
 HOST,PORT = tuple(os.sys.argv[1:3])
 PORT = int(PORT)
 ##HOST = '172.22.158.167'  # The server's hostname or IP address
 ##PORT = 1234  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
-print('Received', repr(data))
+def send_msg(msg):
+    msg=msg.encode('utf-8')
+    totalsent = 0
+    while totalsent < len(msg):
+        sent = s.send(msg[totalsent:])
+        if sent == 0:
+            raise RuntimeError("socket connection broken")
+        totalsent = totalsent + sent
+
+
+while True:
+    send_msg("send a msg")
+    data = s.recv(1024)
+    print('Received', repr(data))
+    time.sleep(1)
+    
--- echo_server.py
+++ echo_server.py
@@ -5,9 +5,10 @@ HOST,PORT = tuple(os.sys.argv[1:3])
 ##HOST = '172.22.156.167'  # Standard loopback interface address (localhost)
 ##PORT = 1234        # Port to listen on (non-privileged ports are > 1023)
 PORT = int(PORT)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+while True:
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
