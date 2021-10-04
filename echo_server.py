b3dafda2f469f17d7a2af4fcb232baba2a0e4ce6
lich97zz <136956344@qq.com>
2021-10-03 22:14:45 -0500

    test

--- echo_server.py
+++ echo_server.py
@@ -1,58 +1,61 @@
+#!/usr/bin/env python3
+
+import sys
 import socket
-import os
+import selectors
+import types
 
-HOST,PORT = tuple(os.sys.argv[1:3])
-##HOST = '172.22.156.167'  # Standard loopback interface address (localhost)
-##PORT = 1234        # Port to listen on (non-privileged ports are > 1023)
-PORT = int(PORT)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen()
-while True:
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            stringdata = data.decode('utf-8')
-            print("Received:"+stringdata)
-            if not data:
-                break
-            data = stringdata.encode('utf-8')
-            conn.sendall(data)
+sel = selectors.DefaultSelector()
 
-##
-##def cli_node_handler(clientsocket,address):
-##
-##    def msg_iter():
-##        global datacnt
-##        buffer=b''
-##        while True:
-##            received = clientsocket.recv(1024)
-##            
-##            with datacnt_mtx:
-##                datacnt += len(received)
-##            
-##            buffer += received
-##                
-##            if not buffer:
-##                break
-##            else:
-##                lines = buffer.split(b'\n')
-##                buffer=lines[-1]
-##                
-##                for i in lines[0:-1]:
-##                    yield i.decode('utf-8')
-##        clientsocket.close()
-##    
-##    msgs=msg_iter()
-##    nodename=next(msgs)
 
-    
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print("accepted connection from", addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
 
-        
-        with msg_storage_mtx:
-            msg_storage.append((cli_time,recv_time,event_hash))
 
-    with print_mtx:
-        print("%s - %s disconnected" % (time.time(), nodename))
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print("closing connection to", data.addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print("echoing", repr(data.outb), "to", data.addr)
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+
+if len(sys.argv) != 3:
+    print("usage:", sys.argv[0], "<host> <port>")
+    sys.exit(1)
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print("listening on", (host, port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
---
+++ simple_client_vm1.py
@@ -0,0 +1,60 @@
+import os, socket
+import time
+import threading
+
+#run on vm1 172.22.156.167 port=1234, send to vm2 172.22.158.167 port=1235
+def client_func():
+    #send
+    def send_msg(msg):
+        msg=msg.encode('utf-8')
+        totalsent = 0
+        while totalsent < len(msg):
+            sent = s.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+    host_ip = "172.22.158.167"
+    port = 1235
+    port = int(port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            s.connect((host_ip, port))
+            break
+        except:
+            print("Can't connect, reconnect in 1 second...")
+            time.sleep(1)
+            continue
+
+    msg_index = 0
+    while True:
+        send_msg('<VM1>:sending message with index:'+str(msg_index)+' \n')
+        time.sleep(2)
+
+    s.close()
+
+def server_func():
+    #receive
+##    host_ip = '172.22.156.167'  # Standard loopback interface address (localhost)
+    port = 1234        # Port to listen on (non-privileged ports are > 1023)
+    port = int(port)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", port))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            data_content = data.decode('utf-8')
+            print("received:"+data_content)
+            if not data:
+                break
+    
+
+send_t = threading.Thread(target=client_func, args=())
+send_t.start()
+
+receive_t = threading.Thread(target=server_func, args=())
+receive_t.start()
+
