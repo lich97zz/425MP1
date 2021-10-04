596a873ba1ed9b4c182f52bb97ecaf0c9345b7ac
lich97zz <136956344@qq.com>
2021-10-04 13:15:50 -0500

    1

--- simple_client_vm3.py
+++ simple_client_vm3.py
@@ -28,7 +28,7 @@ def client_func():
 
     msg_index = 0
     while True:
-        send_msg('<VM2>:sending message with index:'+str(msg_index)+' \n')
+        send_msg('<VM1>:sending message with index:'+str(msg_index)+' \n')
         time.sleep(2)
 
     s.close()
@@ -43,8 +43,8 @@ def client_func1():
             if sent == 0:
                 raise RuntimeError("socket connection broken")
             totalsent = totalsent + sent
-    host_ip = "172.22.94.167"
-    port = 1236
+    host_ip = "172.22.158.167"
+    port = 1235
     port = int(port)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     while True:
@@ -52,13 +52,13 @@ def client_func1():
             s.connect((host_ip, port))
             break
         except:
-            print("Can't connect to node 3, reconnect in 1 second...\n")
+            print("Can't connect to node 2, reconnect in 1 second...\n")
             time.sleep(1)
             continue
 
     msg_index = 0
     while True:
-        send_msg('<VM2>:sending message with index:'+str(msg_index)+' \n')
+        send_msg('<VM1>:sending message with index:'+str(msg_index)+' \n')
         time.sleep(2)
 
     s.close()
@@ -84,18 +84,18 @@ def server_func():
 def server_func1():
     #receive
 ##    host_ip = '172.22.156.167'  # Standard loopback interface address (localhost)
-    port = 1236        # Port to listen on (non-privileged ports are > 1023)
+    port = 1235        # Port to listen on (non-privileged ports are > 1023)
     port = int(port)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("", port))
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print('Connected by node 3 '+addr+"\n")
+        print('Connected by node 2 '+addr+"\n")
         while True:
             data = conn.recv(1024)
             data_content = data.decode('utf-8')
-            print("received fron node 3:"+data_content+"\n")
+            print("received fron node 2:"+data_content+"\n")
             if not data:
                 break    
 
