00cc2d61d54870040d136195705451f2e2325d2b
lich97zz <136956344@qq.com>
2021-10-04 21:53:00 -0500

    1

--- simple_client.py
+++ simple_client.py
@@ -57,7 +57,7 @@ def establish_connection(node_id):
             continue
     return
     
-def server_func():
+def server_func1():
     global self_port
     self_port = int(self_port)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -75,6 +75,59 @@ def server_func():
         conn.close()
     s.close()
         
+def server_func():
+    global self_port
+    host = ''
+    sel = selectors.DefaultSelector()
+
+    def accept_wrapper(sock):
+        conn, addr = sock.accept()  # Should be ready to read
+        print("accepted connection from", addr)
+        conn.setblocking(False)
+        data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        sel.register(conn, events, data=data)
+
+
+    def service_connection(key, mask):
+        sock = key.fileobj
+        data = key.data
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)  # Should be ready to read
+            if recv_data:
+                data.outb += recv_data
+            else:
+                print("closing connection to", data.addr)
+                sel.unregister(sock)
+                sock.close()
+        if mask & selectors.EVENT_WRITE:
+            if data.outb:
+                print("echoing", repr(data.outb), "to", data.addr)
+                sent = sock.send(data.outb)  # Should be ready to write
+                data.outb = data.outb[sent:]
+
+
+
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.bind(('', self_port))
+    lsock.listen()
+    print("listening on", (host, self_port))
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
+
+    try:
+        while True:
+            events = sel.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    accept_wrapper(key.fileobj)
+                else:
+                    service_connection(key, mask)
+    except KeyboardInterrupt:
+        print("caught keyboard interrupt, exiting")
+    finally:
+        sel.close()
+
 
         
     
@@ -97,10 +150,10 @@ def init(file_name):
 
 
 
-file_name = str(os.sys.argv[1])
-self_port = int(os.sys.argv[2])
-##file_name = "config_vm1"
-##self_port = 1234
+##file_name = str(os.sys.argv[1])
+##self_port = int(os.sys.argv[2])
+file_name = "config_vm1"
+self_port = 1234
 connect_num = 0
 name_list = []
 ip_list = []
