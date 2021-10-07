a227a4138165eae595e7d0c8094ddaa13acdee2c
lich97zz <136956344@qq.com>
2021-10-07 14:00:30 -0500

    1

--- simple_client.py
+++ simple_client.py
@@ -93,6 +93,73 @@ def establish_connection(node_id):
             continue
     return
 
+##def server_func():
+##    global self_port
+##    sel = selectors.DefaultSelector()
+##    def accept_wrapper(sock):
+##        conn, addr = sock.accept()
+##        conn.setblocking(False)
+##        data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+##        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+##        sel.register(conn, events, data=data)
+##
+##    def service_connection(key, mask):
+##        sock = key.fileobj
+##        data = key.data
+##        if mask & selectors.EVENT_READ:
+##            recv_data = sock.recv(1024)  # Should be ready to read
+##            if recv_data:
+##                data.outb += recv_data
+##                if not recv_data:
+##                    sel.unregister(s)
+##                    s.close()
+##
+##                lines = data.outb.split(b'#')
+##                received_msg = lines[-1]
+##                info = lines[:-1]
+##            
+####            info = recv_data.decode('utf-8')
+####Received info are merged, modify here
+##            
+##            for elm in info:
+##                elm = elm.decode('utf-8')
+##                if len(elm) < 3:
+##                    continue
+##                
+##                if len(elm.split('|')) < 5:
+##                    print("    Received:"+elm)
+##                    for i in range(50):
+##                        print(" ********************************")
+##
+##                on_receiving(elm)
+##                datacnt += sys.getsizeof(elm)
+##            else:
+##                sel.unregister(sock)
+##                sock.close()
+##        if mask & selectors.EVENT_WRITE:
+##            if data.outb:
+##                data.outb = data.outb[sent:]
+##
+##
+##    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+##    lsock.bind(('', self_port))
+##    lsock.listen()
+##    lsock.setblocking(False)
+##    sel.register(lsock, selectors.EVENT_READ, data=None)
+##
+##    try:
+##        while True:
+##            events = sel.select(timeout=None)
+##            for key, mask in events:
+##                if key.data is None:
+##                    accept_wrapper(key.fileobj)
+##                else:
+##                    service_connection(key, mask)
+##    except KeyboardInterrupt:
+##        print("caught keyboard interrupt, exiting")
+##    finally:
+##        sel.close()
+
 
 def server_func():
     #a many to one receive function
@@ -120,10 +187,12 @@ def server_func():
             if not recv_data:
                 sel.unregister(s)
                 s.close()
-            with receive_mtx:
-                received_msg += recv_data
-                lines = received_msg.split(b'#')
-                received_msg = lines[-1]
+            else:
+##            with receive_mtx:
+                data.outb += recv_data
+
+                lines = data.outb.split(b'#')
+                data.outb = lines[-1]
                 info = lines[:-1]
             
 ##            info = recv_data.decode('utf-8')
